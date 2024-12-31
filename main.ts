243e8943345021b31ce67dbdaffb953abd908287
Nolan0027 <128444566+Nolan0027@users.noreply.github.com>
2024-12-30 23:25:02 -0600

    Restore '12/30/2024, 7:59:07 PM Merge pull request #6 from Nolan0027/merge-6
    Tweaked some textures, overhualed shooting system, etc'

--- main.ts
+++ main.ts
@@ -1,12 +1,5 @@
-namespace SpriteKind {
-    export const UI = SpriteKind.create()
-}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    HandUI.setImage(assets.image`Hand_gun2`)
     _2 = darts.create(assets.image`3`, SpriteKind.Projectile, Render.getRenderSpriteInstance().x, Render.getRenderSpriteInstance().y)
-    _2.follow(_3)
-    pause(400)
-    HandUI.setImage(assets.image`Hand_gun`)
     _2.angle = Render.getAttribute(Render.attribute.dirX)
     _2.angleRate = 0
     _2.throwDart()
@@ -27,32 +20,20 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, oth
     }
 })
 let _2B: Sprite = null
-let _2: Dart = null
-let HandUI: Sprite = null
 let _3: Sprite = null
+let _2: Dart = null
 let _1 = Render.getRenderSpriteVariable()
 _3 = sprites.create(assets.image`4`, SpriteKind.Enemy)
-HandUI = sprites.create(assets.image`Hand_gun`, SpriteKind.UI)
-HandUI.setFlag(SpriteFlag.RelativeToCamera, true)
-HandUI.scale = 3.4
-HandUI.setPosition(81, 103)
 Render.move(Render.getRenderSpriteInstance(), 100)
 _1.setPosition(270, 250)
 _3.setPosition(260, 390)
 tiles.setCurrentTilemap(tilemap`Level`)
-scene.setBackgroundImage(assets.image`Bg`)
 info.setLife(10)
-music.play(music.createSong(assets.song`theme1`), music.PlaybackMode.UntilDone)
-music.play(music.createSong(assets.song`theme2`), music.PlaybackMode.UntilDone)
-music.play(music.createSong(assets.song`Drums`), music.PlaybackMode.LoopingInBackground)
 forever(function () {
-    music.play(music.createSong(assets.song`theme1`), music.PlaybackMode.UntilDone)
-    music.play(music.createSong(assets.song`theme2`), music.PlaybackMode.UntilDone)
-})
-game.onUpdateInterval(10000, function () {
-    sprites.destroyAllSpritesOfKind(SpriteKind.Projectile)
-})
-game.onUpdateInterval(4500, function () {
+    pause(5000)
     _2B = sprites.createProjectileFromSprite(assets.image`5`, _3, 100, 0)
     _2B.follow(_1)
+})
+game.onUpdateInterval(20000, function () {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Projectile)
 })
